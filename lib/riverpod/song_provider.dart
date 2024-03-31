9ec6aae5f2d1609ff72b8ac88d9339043bd0adc2
dominhsang-34 <dosangnd2020@gmail.com>
2024-03-31 19:16:54 +0700

    backup push

--- lib/riverpod/song_provider.dart
+++ lib/riverpod/song_provider.dart
@@ -1,66 +1,58 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:webtoon/home/homeui.dart';
+import 'package:webtoon/riverpod/song.dart';
+
+//Song Data
+List<Song> songs = [
+  Song(
+      id: "1",
+      title: "Album",
+      artist: "Artist",
+      audioUrl: "assets/audios/TTL.mp3",
+      imgUrl: "assets/icon.png"),
+  Song(
+      id: "2",
+      title: "title",
+      artist: "Artist2",
+      audioUrl: "assets/audios/vokichcuaem.mp3",
+      imgUrl: "assets/artwork.jpg"),
+  Song(
+      id: "3",
+      title: "title3",
+      artist: "Artist3",
+      audioUrl: "assets/audios/anhdaungo.mp3",
+      imgUrl: "assets/icon.png"),
+  Song(
+      id: "4",
+      title: "title4",
+      artist: "Artist4",
+      audioUrl: "assets/audios/Stay.mp3",
+      imgUrl: "assets/icon.png"),
+  Song(
+      id: "5",
+      title: "title5",
+      artist: "Artist5",
+      audioUrl: "assets/audios/HONGKONG1.mp3",
+      imgUrl: "assets/icon.png"),
+];
 
 class AudioHandler extends Notifier<AudioPlayer> {
-  //data for testing purpose, will refactor later
-  final _playlist = ConcatenatingAudioSource(children: [
-    AudioSource.asset('assets/audios/TTL.mp3',
-        tag: const MediaItem(
-          id: '1',
+  List<AudioSource> playlist = songs.map((song) {
+      return AudioSource.asset(
+        song.audioUrl,
+        tag: MediaItem(
+          id: song.id,
           album: "Album",
-          title: "Title",
-          artist: "Artist",
-          // artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-        )),
-    AudioSource.asset('assets/audios/vokichcuaem.mp3',
-        tag: const MediaItem(
-          id: '1',
-          album: "Album",
-          title: "Title",
-          artist: "Artist",
-          // artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-        )),
-    AudioSource.asset('assets/audios/anhdaungo.mp3',
-        tag: const MediaItem(
-          id: '1',
-          album: "Album",
-          title: "Title",
-          artist: "Artist",
-          // artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-        )),
-    AudioSource.asset('assets/audios/Stay.mp3',
-        tag: const MediaItem(
-          id: '1',
-          album: "Album",
-          title: "Title",
-          artist: "Artist",
-          // artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-        )),
-    AudioSource.asset('assets/audios/HONGKONG1.mp3',
-        tag: const MediaItem(
-          id: '1',
-          album: "Album",
-          title: "Title",
-          artist: "Artist",
-          // artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-        )),
-    AudioSource.asset('assets/audios/SOLDOUT.mp3',
-        tag: const MediaItem(
-          id: '1',
-          album: "Album",
-          title: "Title",
-          artist: "Artist",
-          // artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-        )),
-    // AudioSource.uri(Uri.parse('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'), tag: MediaItem(
-    //   id: '1',
-    //   album: "Album",
-    //   title: "Title",
-    //   artist: "Artist",
-    //   artUri: Uri.parse("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"),
-    // )),
-  ]);
+          title: song.title,
+          artist: song.artist,
+        ),
+      );
+    }).toList();
+
+  
+  late final ConcatenatingAudioSource _playlist = ConcatenatingAudioSource(children: playlist);
 
   @override
   AudioPlayer build() {
