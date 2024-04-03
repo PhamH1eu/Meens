11df42ba77d502fb967e8c54b05a4f3efab6f8d3
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2024-04-03 11:27:36 +0700

    feat: Test Firebase cloud storage, restructure folder.

--- lib/riverpod/song_provider.dart
+++ lib/riverpod/song_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:just_audio/just_audio.dart';
@@ -10,58 +8,51 @@ List<Song> playlist = [
   const Song(
     title: 'TTL - Listen 2',
     artist: 'T-ARA',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/TTL.mp3',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Ftara.jpg?alt=media&token=041b1471-4a1b-4c53-87f8-abc1cfb875e7',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2FTTL.mp3?alt=media&token=c3c438fa-af4c-4a15-8045-e35bc4afa966',
   ),
   const Song(
     title: 'Vo kich cua em',
     artist: 'Huong Ly',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/vokichcuaem.mp3',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Fhophongan.jpg?alt=media&token=fef5c778-fa7c-49ee-a34c-c7e99963e48d',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2Fvokichcuaem.mp3?alt=media&token=b82a3baa-5f94-4f9e-81a0-f41e4c45d7c6',
   ),
   const Song(
-    title: 'Stay',
-    artist: 'Kid',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/Stay.mp3',
+    title: 'Atlantis',
+    artist: 'Seafret',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Fseafret.jpg?alt=media&token=3612b6f5-6b29-4121-b619-520cc93b86d9',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2Fatlantis.mp3?alt=media&token=40e223a9-64cf-44ec-ad73-f4bb78125323',
   ),
   const Song(
-    title: 'Sold Out',
-    artist: 'Malenia',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/SOLDOUT.mp3',
+    title: 'Fly a letter the wind',
+    artist: 'July',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Fjuly.jpg?alt=media&token=f7362d5e-22fa-4ae9-bc7d-60fad9131cea',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2Fflyalettertothewind.mp3?alt=media&token=029d1a32-0a1b-4acd-a5de-52edd301541f',
   ),
   const Song(
-    title: 'HONGKONG1',
-    artist: 'Miquella',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/HONGKONG1.mp3',
+    title: 'GODS',
+    artist: 'New Jeans',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Fnewjeans.jpg?alt=media&token=d57894c6-cbf0-4f2f-97cd-bbd43bafdd19',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2Fgods.mp3?alt=media&token=9266df98-a655-42c9-b751-c24ef8cb9b0b',
   ),
   const Song(
-    title: 'Anh Dau Ngo',
-    artist: 'Marika',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/anhdaungo.mp3',
+    title: 'Viva La Vida',
+    artist: 'Coldplay',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Fcoldplay.jpg?alt=media&token=4c15b070-dac9-4532-b340-fee903c3e821',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2Fvivalavida.mp3?alt=media&token=4b30c354-4086-4a67-9735-3b99ae7c9293',
   ),
   const Song(
-    title: 'Mascara',
-    artist: 'Godwyn',
-    imageUrl:
-        'https://assetsio.gnwcdn.com/elden-ring-ranni.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
-    songPath: 'assets/audios/Mascara Lung Linh.mp3',
+    title: 'Your lie in april',
+    artist: 'Hikaru Nara',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/artist%2Fhikarunara.jpg?alt=media&token=59cad8b6-2de0-492b-b351-3db33eb7b5f6',
+    songPath: 'https://firebasestorage.googleapis.com/v0/b/webtoon-b9373.appspot.com/o/song%2Fyourlieinapril.mp3?alt=media&token=8bc5b7b6-8366-4d97-9f77-3ed341088dfb',
   ),
 ];
 
 final _playlist = ConcatenatingAudioSource(
   useLazyPreparation: true,
   children: playlist
-      .map((song) => AudioSource.asset(song.songPath!,
+      .map((song) => AudioSource.uri(Uri.parse(song.songPath!),
           tag: MediaItem(
             id: song.songPath!,
             artist: song.artist,
@@ -84,6 +75,10 @@ class AudioHandlers extends ChangeNotifier {
     return playlist[audioPlayer.currentIndex ?? 0];
   }
 
+  List<String> get imgList {
+    return playlist.map((e) => e.imageUrl).toList();
+  }
+
   Future<void> next() async {
     if (audioPlayer.hasNext) {
       await audioPlayer.seekToNext();
@@ -100,11 +95,12 @@ class AudioHandlers extends ChangeNotifier {
 
   Future<void> changeTo(int index) async {
     await audioPlayer.seek(Duration.zero, index: index);
-    log("changeTo index: ${audioPlayer.currentIndex}");
     notifyListeners();
   }
 
   //Haven't used those functions yet
+  //
+  //
   bool setShuffleMode() {
     audioPlayer.setShuffleModeEnabled(!audioPlayer.shuffleModeEnabled);
     notifyListeners();
---
+++ lib/screen/faqs/faq_screen.dart
@@ -0,0 +1,404 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:webtoon/utilities/fonts.dart';
+
+import '../../riverpod/tab.dart';
+
+class FAQ extends StatelessWidget {
+  const FAQ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                ref.invalidate(countProvider);
+              },
+            ),
+            title: Text(
+              'FAQ',
+              style: TextStyle(
+                  fontWeight: CustomColors.extraBold,
+                  color: Theme.of(context).primaryColor),
+            ),
+            centerTitle: true,
+          ),
+          body: Container(
+            padding: const EdgeInsets.only(bottom: 70),
+            color: Theme.of(context).focusColor,
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16, left: 16),
+                    child: Text(
+                      "Downloading",
+                      style: TextStyle(
+                          fontWeight: CustomColors.extraBold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "Can i download or cache my music",
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+                    children: [
+                      Text(
+                        "Yes, Meens allows you downloading or caching music at your devices. In order to play music, you must have a working internet connection first in order to download",
+                        style: TextStyle(
+                            fontFamily: 'Gilroy',
+                            color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text("Playback",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: CustomColors.extraBold,
+                          fontSize: 20,
+                        )),
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "Does Musi use mydata",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I enable crossfade?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I adjust playback speed?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      "Playlists",
+                      style: TextStyle(
+                          fontWeight: CustomColors.extraBold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I add tracks to my playlist",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I create a new playlist",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I delete a playlist",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      "Tracks",
+                      style: TextStyle(
+                          fontWeight: CustomColors.extraBold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I delete tracks?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I edit a track name or artist?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I play age restricted tracks?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "sii",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      "Transfer/Backup",
+                      style: TextStyle(
+                          fontWeight: CustomColors.extraBold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  ExpansionTile(
+                    collapsedBackgroundColor:
+                        Theme.of(context).scaffoldBackgroundColor,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    shape: const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        ),
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                    expandedAlignment: Alignment.centerLeft,
+                    childrenPadding: const EdgeInsets.only(left: 16, right: 16),
+                    title: Text(
+                      "How can I transfer my library and playlists to a new device?",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    children: [
+                      Text(
+                        "Yes, Meens allows you downloading or caching music at your devices. In order to play music, you must have a working internet connection first in order to download",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
