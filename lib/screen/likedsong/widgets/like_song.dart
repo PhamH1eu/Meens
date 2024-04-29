beedd37fa9c988081724aa545c74e208d129c541
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2024-04-29 09:37:48 +0700

    Update like_song.dart

--- lib/screen/likedsong/widgets/like_song.dart
+++ lib/screen/likedsong/widgets/like_song.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/utilities/fonts.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:webtoon/riverpod/song_provider.dart';
 import '../../../model/song.dart';
 
-class LikeSongInfo extends StatefulWidget {
+class LikeSongInfo extends ConsumerStatefulWidget {
   const LikeSongInfo({super.key, required this.song});
 
   final Song song;
 
   @override
-  State<LikeSongInfo> createState() => _LikeSongInfoState();
+  _LikeSongInfoState createState() => _LikeSongInfoState();
 }
 
-class _LikeSongInfoState extends State<LikeSongInfo> {
+class _LikeSongInfoState extends ConsumerState<LikeSongInfo> {
   Color glowColor = Colors.black;
 
   @override
@@ -29,44 +30,50 @@ class _LikeSongInfoState extends State<LikeSongInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        SizedBox(
-          height: 140,
-          width: 140,
-          child: Stack(children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  image: NetworkImage(widget.song.imageUrl),
-                  fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        ref.watch(audioHandlerProvider.notifier).setSong(widget.song);
+        Navigator.of(context).pushNamed('/play');
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 140,
+            width: 140,
+            child: Stack(children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage(widget.song.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
+            ]),
+          ),
+          const SizedBox(height: 9),
+          Text(
+            widget.song.title,
+            style: TextStyle(
+              fontWeight: CustomColors.semiBold,
+              color: Theme.of(context).primaryColor,
+              fontFamily: 'Gilroy',
+              fontSize: 12,
             ),
-          ]),
-        ),
-        const SizedBox(height: 9),
-        Text(
-          widget.song.title,
-          style: TextStyle(
-            fontWeight: CustomColors.semiBold,
-            color: Theme.of(context).primaryColor,
-            fontFamily: 'Gilroy',
-            fontSize: 12,
           ),
-        ),
-        const SizedBox(height: 4),
-        Text(
-          widget.song.artist,
-          style: TextStyle(
-            color: Theme.of(context).secondaryHeaderColor,
-            fontFamily: 'Gilroy',
-            fontSize: 8,
+          const SizedBox(height: 4),
+          Text(
+            widget.song.artist,
+            style: TextStyle(
+              color: Theme.of(context).secondaryHeaderColor,
+              fontFamily: 'Gilroy',
+              fontSize: 8,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
