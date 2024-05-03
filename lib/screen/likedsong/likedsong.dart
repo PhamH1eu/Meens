4d008ca6caf78f1c50a2fff25f61b2f37369a620
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2024-05-03 22:35:42 +0700

    Update likedsong.dart

--- lib/screen/likedsong/likedsong.dart
+++ lib/screen/likedsong/likedsong.dart
@@ -1,14 +1,15 @@
-import 'dart:html';
 
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:webtoon/screen/likedsong/widgets/like_song.dart';
 import 'package:webtoon/utilities/fonts.dart';
+import 'package:webtoon/firebase/cloud_store/store.dart';
+import '../../model/song.dart';
 
-import '../../riverpod/firebase_provider.dart';
-import '../home/homeui.dart';
 import '../../riverpod/tab.dart';
-
 
 
 class LikedSong extends ConsumerWidget {
@@ -16,71 +17,128 @@ class LikedSong extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final likedsong = ref.watch(likedSongsProvider);
-    return Consumer(
-      builder: (context, ref, child) {
-        return Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () {
-                ref.invalidate(countProvider);
-              },
-            ),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.filter_list),
-                onPressed: () {
-                  // Xử lý khi nút "filter" được nhấn
-                },
-              ),
-            ],
-          ),
-          body: Padding(
-            padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 20),
-                Text(
-                  'Liked Songs',
-                  style: TextStyle(
-                      fontWeight: CustomColors.extraBold,
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 25,
-                      fontFamily: 'Gilroy'),
-                ),
-                const SizedBox(height: 20),
-                 Expanded(
-                    child: likedsong.when(
-                        data: (likedSongs) {
-                          return GridView.builder(
-                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              childAspectRatio: MediaQuery.of(context).size.width /
-                                  (MediaQuery.of(context).size.height / 1.65),
-                            ),
-                            itemCount: likedSongs.length,
-                            itemBuilder: (context, index){
-                              return LikeSongInfo(song: likedSongs[index]);
+
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('Users')
+            .doc(FirebaseAuth.instance.currentUser!.email)
+            .snapshots(),
+        builder: (context,snapshot) {
+          while (snapshot.connectionState == ConnectionState.waiting ||
+              !snapshot.hasData) {
+            return Center(
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor,
+                ));
+          }
+          List<String> listName = [];
+
+          try {
+            listName = List.from(snapshot.data!['likedSong']);
+          } catch (e) {
+            //this is normal, it is laggy a bit when the user is first time
+            return Scaffold(
+              body: Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  )),
+            );
+          }
+          return StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Songs')
+                  .where('title', whereIn: listName)
+                  .snapshots(),
+              builder: (context , snapshot) {
+                while (snapshot.connectionState == ConnectionState.waiting ||
+                    !snapshot.hasData) {
+                  return Scaffold(
+                    body: Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).primaryColor,
+                        )),
+                  );
+                }
+
+                final List<Song> likedsong = [];
+                try {
+                  for (var docSnapshot in snapshot.data!.docs) {
+                    Storage.getSongUrl(docSnapshot['title']).then((songPath) {
+                      Storage.getSongAvatarUrl(docSnapshot['title']).then((imageUrl) {
+                        likedsong.add(Song.fromJson(docSnapshot, imageUrl, songPath));
+                        print(docSnapshot.toString());
+                      });
+                    });
+                  }
+                } catch (e) {
+                  //this is normal, it is laggy a bit when the user is first time
+                  return Scaffold(
+                    body: Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).primaryColor,
+                        )),
+                  );
+                }
+                return Consumer(
+                  builder: (context, ref, child) {
+                    return Scaffold(
+                      appBar: AppBar(
+                        leading: IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () {
+                            ref.invalidate(countProvider);
+                          },
+                        ),
+                        actions: <Widget>[
+                          IconButton(
+                            icon: const Icon(Icons.filter_list),
+                            onPressed: () {
+                              // Xử lý khi nút "filter" được nhấn
                             },
-                          );
-                        },
-                      loading: () => Center(
-                          child: CircularProgressIndicator(
-                            color: Theme.of(context).primaryColor,
-                          )),
-                      error: (error, stack) => Text('Error: $error'),)
-                  ),
+                          ),
+                        ],
+                      ),
+                      body: Padding(
+                        padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const SizedBox(height: 20),
+                            Text(
+                              'Liked Songs',
+                              style: TextStyle(
+                                  fontWeight: CustomColors.extraBold,
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 25,
+                                  fontFamily: 'Gilroy'),
+                            ),
+                            const SizedBox(height: 20),
+                            Expanded(
+                                child: GridView.builder(
+                                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio: MediaQuery.of(context).size.width /
+                                        (MediaQuery.of(context).size.height / 1.65),
+                                  ),
+                                  itemCount: likedsong.length,
+                                  itemBuilder: (context, index){
+                                    return LikeSongInfo(song: likedsong[index]);
+                                  },
+                                )
+                            ),
 
 
-                const SizedBox(height: 20),
-              ],
-            ),
-          ),
-        );
-      },
+                            const SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+          );
+        }
     );
   }
 }
